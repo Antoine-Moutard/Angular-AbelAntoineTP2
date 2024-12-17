f46c6f2d3961e6500cd156d31ff97e96c41cae98
Antoine-Moutard <amoutard368@gmail.com>
2024-12-17 15:12:43 +0100

    styling

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, Router],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
@@ -18,11 +20,15 @@ toggleEmail(event: any) {
   this.hideEmail = event.target.checked;
 }
 
+constructor(private router: Router) {}
+
 onSubmit(form: any) {
   if (form.valid) {
     alert('Le formulaire est valide');
     localStorage.setItem('contactData', JSON.stringify(form.value));
     form.reset();
+    this.router.navigate(['/manage']);
+
   }
 }
 }
