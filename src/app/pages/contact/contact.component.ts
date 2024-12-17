b39435ddb2ca8e50fc7d03f179a72e62c0748fed
Antoine-Moutard <amoutard368@gmail.com>
2024-12-17 15:14:19 +0100

    fix

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -2,14 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { Router } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule, Router],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
@@ -20,14 +19,12 @@ toggleEmail(event: any) {
   this.hideEmail = event.target.checked;
 }
 
-constructor(private router: Router) {}
 
 onSubmit(form: any) {
   if (form.valid) {
     alert('Le formulaire est valide');
     localStorage.setItem('contactData', JSON.stringify(form.value));
     form.reset();
-    this.router.navigate(['/manage']);
 
   }
 }
